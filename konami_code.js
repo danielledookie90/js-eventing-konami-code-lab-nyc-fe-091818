519c88001ea4424d07ab62dba1845db5ed8fde69
Danielle Dookie <danielledookie90@gmail.com>
2018-10-31 01:01:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let tracker = 0
 
 function onKeyDown(event){
-  const keycode = event.which
+  const keycode = event.which ||event.location
   
   if (keycode === code[tracker]) {
     tracker = tracker + 1
